acfe9ab4fa8aac9526c8efc2ad270fab827d59cf
shiyunkhoo <shiyun.khoo@gmail.com>
2022-08-21 14:05:39 +0800

    debugging netlify build fails

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '../styles/globals.css';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function MyApp({Component, pageProps}) {
-  return <Component {...pageProps} />
-};
+  return <Component {...pageProps} />;
 
 export default MyApp;
