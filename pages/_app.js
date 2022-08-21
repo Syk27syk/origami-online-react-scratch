669aa68a320757a1d7890fcbbaf77624dfe9f195
shiyunkhoo <shiyun.khoo@gmail.com>
2022-08-21 13:32:37 +0800

    debugging netlify deploy fails

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   return <Component {...pageProps} />
-}
+};
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default function Home() {
   return (
